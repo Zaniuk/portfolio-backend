32a5e5759f6d199fbdc1e0418f1d4a3178e7c483
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-07-10 02:02:32 -0300

    added CORS

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const dotenv = require('dotenv')
 const postsRouter = require('./routes/posts')
 dotenv.config()
@@ -8,7 +9,7 @@ const bodyParser = require('body-parser')
 
 const app = express()
 app.use(bodyParser.json())
-
+app.use(cors())
 
 
 app.use(postsRouter)
