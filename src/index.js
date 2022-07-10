efa9f43a13b10f1c6428aea3527ed349ede0884b
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-07-10 01:35:20 -0300

    Created post basic post structure

--- src/index.js
+++ src/index.js
@@ -2,19 +2,21 @@ const express = require('express')
 const dotenv = require('dotenv')
 const postsRouter = require('./routes/posts')
 dotenv.config()
-const connectionUrl = process.env.CONNECTION_URL
+const PORT = process.env.PORT || 80
 
 const bodyParser = require('body-parser')
+
 const app = express()
+app.use(bodyParser.json())
+
+
+
 app.use(postsRouter)
-
-app.use(bodyParser.json({ type: 'application/*+json' }))
-
 app.get('/', (_req, res) => {
     res.send('Hola Mundo')
 })
 app.get('/', postsRouter)
 
-app.listen(80, (_req, _res)=>{
+app.listen(PORT, (_req, _res)=>{
     console.log('Server Started')
 })
--- src/routes/posts.js
+++ src/routes/posts.js
@@ -2,15 +2,43 @@ const express = require('express')
 const router = express.Router()
 const dotenv = require('dotenv')
 const mongoose = require('mongoose')
+const Post = require('../schema/Post')
 dotenv.config()
+
 const connectionUrl = process.env.CONNECTION_URL
 mongoose.connect(connectionUrl, (err) => {
     if(err) throw err
     else console.log('DB Connected')
 })
 
-router.get('/posts', (_req, res)=> {
-    res.send('Posts')
+router.get('/posts', (_req, __res)=> {
+    Post.find({})
+        .then(res => {
+            __res.json(res)
+        })
+})
+
+router.post('/posts', (req, res) => {
+    async function createPost(){
+        const request = req.body
+        const post = new Post({
+            title: request.title,
+            body: request.body,
+            createdAt: new Date(),
+            tags: request.tags,
+            img: request.img
+        })
+        post.validate((err) => {
+            if(err){
+                res.json(err)
+            }else{
+                res.json(post)
+                post.save()
+            }
+        })
+        
+    }
+    createPost()
 })
 
 module.exports = router;
