d13d2eac3146ffe71d6051da64fa696c22a6b755
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-11-17 15:56:43 -0300

    refactor auth

--- src/routes/verifyToken.js
+++ src/routes/verifyToken.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken')
 
 module.exports = function (req, res, next) {
-    const token = req.header('auth-token')
+    const token = req.header('Authorization') || req.header('authorization')
     if (!token) return res.status(401).send('Access denied')
 
     try {
